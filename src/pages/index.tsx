7247a324543a6ddb02187733a08963b112d2d79d
Bruno De Masi <bruno.demasi@terras.agr.br>
2021-05-21 01:38:54 -0300

    feat: home as SSG (Server Static Generation)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
 import { SubscribeButton } from '../components/SubscribeButton';
@@ -29,7 +29,7 @@ export default function Home({ product }: HomeProps) {
             <span>for {product.amount}/month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
@@ -38,10 +38,8 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const price = await stripe.prices.retrieve('price_1ItPZ5E0AgZULgqwV02AZbd6', {
-    expand: ['product']
-  });
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1ItPZ5E0AgZULgqwV02AZbd6');
 
   const product = {
     priceId: price.id,
@@ -54,6 +52,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
   }
 }
